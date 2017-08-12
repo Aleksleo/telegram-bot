afdbda29ec9d311a84cadc829fd22dbf09bf36d2
Aleksey Leonidov <leonidov.aleksey.v@gmail.com>
2017-08-12 15:25:30 +0700

    Initial commit

---
+++ constants.py
@@ -0,0 +1,2 @@
+token = "250060771:AAFBaFuugw_118LyPyrEArPrrMU-EUfyg84"
+
---
+++ main.py
@@ -0,0 +1,7 @@
+# Telegram Bot Main file v0.1, Leonidov Aleksey
+
+import telebot
+import constants
+
+bot = telebot.TeleBot(constants.token)
+

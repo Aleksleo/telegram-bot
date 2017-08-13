c30b4e61f287259d7258a9571c1f3e771788547d
Aleksey Leonidov <leonidov.aleksey.v@gmail.com>
2017-08-13 12:09:41 +0600

    Add processing of commands with logging in console

--- main.py
+++ main.py
@@ -5,22 +5,32 @@ import constants
 
 bot = telebot.TeleBot(constants.token)
 
-# bot.send_message(211695408, "Hello, I'm your personal assistant")
+# Logging into console
+print(bot.get_me())
+
+def log(message, answer):
+    print("\n --------------")
+    from datetime import datetime
+    print(datetime.now())
+    print("Message from {0} {1}. (id = {2}) \n Text: {3}".format(message.from_user.first_name,
+                                                                 message.from_user.last_name,
+                                                                 str(message.from_user.id),
+                                                                 message.text))
+    print(answer)
+
+
+@bot.message_handler(commands=['install'])
+def handle_text(message):
+    answer = """Type 
+    /install <package> <args>"""
+    bot.send_message(message.chat.id, answer)
+    log(message, answer)
 
 @bot.message_handler(content_types=['text'])
 def handle_text(message):
+    answer = "Hello, I'm your personal assistant. How can I help you?"
     if message.text == "Hello":
-        bot.send_message(message.chat.id, "Hello, I'm your personal assistant. How can I help you?")
-
-
-"""
-# Пока что не работает
-
-@bot.message_handler(content_types=['commands'])
-def handle_command(message):
-    print("Пришла команда")
-    if message.text == "/install":
-        bot.send_message(message.chat.id, "`Type <b>/install</b> <i><package> <args></i>`")
-"""
+        bot.send_message(message.chat.id, answer)
+        log(message,answer)
 
 bot.polling(none_stop=True, interval=0)

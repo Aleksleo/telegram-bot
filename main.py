6b4ecd02f7e956fe83fd500128677b8e201502b4
Aleksey Leonidov <leonidov.aleksey.v@gmail.com>
2017-08-16 18:09:33 +0600

    Create main() function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Telegram Bot Main file v0.1, Leonidov Aleksey
+# Telegram Bot Main file v0.2, Leonidov Aleksey
 
 import telebot
 import constants
@@ -6,58 +6,62 @@ import psutil
 import humanize
 
 
-bot = telebot.TeleBot(constants.token)
+def main():
 
-# Logging into console
-print(bot.get_me())
+    bot = telebot.TeleBot(constants.token)
 
-def log(message, answer):
-    print("\n --------------")
-    from datetime import datetime
-    print(datetime.now())
-    print("Message from {0} {1}. (id = {2}) \n Text: {3}".format(message.from_user.first_name,
-                                                                 message.from_user.last_name,
-                                                                 str(message.from_user.id),
-                                                                 message.text))
-    print(answer)
+    # Logging into console
+    print(bot.get_me())
 
-# Less- or greater-than sign replacement function
+    def log(message, answer):
+        print("\n --------------")
+        from datetime import datetime
+        print(datetime.now())
+        print("Message from {0} {1}. (id = {2}) \n Text: {3}".format(message.from_user.first_name,
+                                                                     message.from_user.last_name,
+                                                                     str(message.from_user.id),
+                                                                     message.text))
+        print(answer)
 
-def replace_tag(value):
-    return value.replace('<', "&lt;").replace('>', "&gt;")
+    # Less- or greater-than sign replacement function
+    def replace_tag(value):
+        return value.replace('<', "&lt;").replace('>', "&gt;")
 
-@bot.message_handler(commands=['install'])
-def handle_text(message):
-    answer = "/install <b>" + replace_tag("<package> <args>") + "</b>"
-    answer += "\n\n<code>Packages:\n linux middleware release\n linux middleware beta</code>"
-    bot.send_message(message.chat.id, answer, parse_mode="HTML")
-    log(message, answer)
-
-@bot.message_handler(commands=['id'])
-def handle_text(message):
-    if message.chat.type == "private":
-        answer = "Your chat ID: <b>" + str(message.chat.id) + "</b>"
-        bot.send_message(message.chat.id, answer, parse_mode="HTML")
-        log(message, answer)
-    elif message.chat.type == "group" or "supergroup":
-        answer = "Group ID: <b>" + str(message.chat.id) + "</b>"
+    @bot.message_handler(commands=['install'])
+    def handle_text(message):
+        answer = "/install <b>" + replace_tag("<package> <args>") + "</b>"
+        answer += "\n\n<code>Packages:\n linux middleware release\n linux middleware beta</code>"
         bot.send_message(message.chat.id, answer, parse_mode="HTML")
         log(message, answer)
 
-@bot.message_handler(commands=['status'])
-def handle_text(message):
-    answer = '<b>I\'m alive!</b>\n<pre>CPU: {}% ({} threads)\nRAM: {}% ({} total)</pre>'.format(
-        psutil.cpu_percent(), psutil.cpu_count(),
-        psutil.virtual_memory().percent,
-        humanize.naturalsize(psutil.virtual_memory().total, binary=True, gnu=True))
-    bot.send_message(message.chat.id, answer, parse_mode="HTML")
-    log(message, answer)
+    @bot.message_handler(commands=['id'])
+    def handle_text(message):
+        if message.chat.type == "private":
+            answer = "Your chat ID: <b>" + str(message.chat.id) + "</b>"
+            bot.send_message(message.chat.id, answer, parse_mode="HTML")
+            log(message, answer)
+        elif message.chat.type == "group" or "supergroup":
+            answer = "Group ID: <b>" + str(message.chat.id) + "</b>"
+            bot.send_message(message.chat.id, answer, parse_mode="HTML")
+            log(message, answer)
 
-@bot.message_handler(content_types=['text'])
-def handle_text(message):
-    answer = "Hello, I'm your personal assistant. How can I help you?"
-    if message.text == "Hello":
+    @bot.message_handler(commands=['status'])
+    def handle_text(message):
+        answer = '<b>I\'m alive!</b>\n<pre>CPU: {}% ({} threads)\nRAM: {}% ({} total)</pre>'.format(
+            psutil.cpu_percent(), psutil.cpu_count(),
+            psutil.virtual_memory().percent,
+            humanize.naturalsize(psutil.virtual_memory().total, binary=True, gnu=True))
         bot.send_message(message.chat.id, answer, parse_mode="HTML")
         log(message, answer)
 
-bot.polling(none_stop=True, interval=0)
+    @bot.message_handler(content_types=['text'])
+    def handle_text(message):
+        answer = "Hello, I'm your personal assistant. How can I help you?"
+        if message.text == "Hello":
+            bot.send_message(message.chat.id, answer, parse_mode="HTML")
+            log(message, answer)
+
+    bot.polling(none_stop=True, interval=0)
+
+if __name__ == "__main__":
+    main()
